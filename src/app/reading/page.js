7934bd0fe7370ca70f6cc6e1043d472f82e018f9
Amarjeet Singh <amarjeetsingh23833@gmail.com>
2026-07-26 10:50:30 +0530

    fix typo

--- src/app/reading/page.js
+++ src/app/reading/page.js
@@ -4,10 +4,14 @@ import BookshelfCarousel from '@/components/BookshelfCarousel';
 export const metadata = {
   title: 'Reading',
 };
+const sortedBooks = [...books].sort(
+  (a, b) => new Date(a.readDate) - new Date(b.readDate)
+);
+
 export default function Reading() {
   return (
     <main className="mt-[50px] md:ml-[330px] max-w-[760px] pt-[80px] md:pt-[20px] px-[20px] font-lora">
-      <BookshelfCarousel books={books} infinite={true} />
+      <BookshelfCarousel books={sortedBooks} infinite={true} />
     </main>
   );
 }
